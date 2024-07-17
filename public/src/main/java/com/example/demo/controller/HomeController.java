171dd397f32386321e51c2eeccc224e8fc1a0861
Ugina <86170855+SickSect@users.noreply.github.com>
2024-07-17 23:37:30 +0300

    cfg process

--- public/src/main/java/com/example/demo/controller/HomeController.java
+++ public/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,9 +1,27 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HomeController {
+
+    @GetMapping("/home")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Hello to home");
+    }
+
+    @GetMapping("/home/admin")
+    public ResponseEntity<String> homeAdmin() {
+        return ResponseEntity.ok("Hello to admin home");
+    }
+
+    @GetMapping("/home/user")
+    public ResponseEntity<String> homeUser() {
+        return ResponseEntity.ok("Hello to user home");
+    }
+
 }
